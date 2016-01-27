8217125fc3b58b53af69e2a9cf0b702fbd17571d
hoanvokim <TihHon@16LH>
2016-01-27 07:49:29 +0700

    config ci project with multi-apps. localhost/baseafood for frontend page. localhost/baseafood/admin.php for backend page (admin dashboard)

---
+++ backend/views/dashboard/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hoanvo
+ * Date: 1/27/16
+ * Time: 7:38 AM
+ */
+echo "this is backend page";
---
+++ frontend/views/home/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hoanvo
+ * Date: 1/27/16
+ * Time: 7:35 AM
+ */
+echo "this is front end page";
