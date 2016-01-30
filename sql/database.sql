bdafdc065e78cdaf19e08fbfdd1cad59558bcc17
NhuTran <trandinhnhubk@gmail.com>
2016-01-31 01:21:58 +0700

    create menu,category tables

--- sql/database.sql
+++ sql/database.sql
@@ -6,4 +6,22 @@ CREATE TABLE `users` (
  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
-insert into users (username, password) values ('supperadmin', MD5('supersecret'));
+insert into users (username, password) values ('superadmin', MD5('supersecret'));
+
+Create table `menu`(
+	`id` tinyint not null auto_increment,
+	`name` nvarchar(100) not null,
+	`fk_parent` tinyint ,
+	`url` varchar(1000),
+	PRIMARY KEY (`id`)
+)ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+create table `category`(
+   `id` tinyint not null auto_increment,
+	`name` nvarchar(100) not null,
+	`fk_parent` tinyint ,
+	PRIMARY KEY (`id`),
+	INDEX `category_parent_index` (`fk_parent`),
+    FOREIGN KEY (`fk_parent`) 
+        REFERENCES `category`(`id`)
+)ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
