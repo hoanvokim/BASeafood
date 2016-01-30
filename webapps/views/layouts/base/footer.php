11bd133ece8cabf33582753f46ebdfc9449002f6
NhuTran <trandinhnhubk@gmail.com>
2016-01-30 10:27:02 +0700

    config database

--- webapps/views/layouts/base/footer.php
+++ webapps/views/layouts/base/footer.php
@@ -7,14 +7,19 @@
                 </h1>
                 <address>
                     <p><i class="fa fa-home pr-10"></i>Address: No.XXXXXX street</p>
+
                     <p><i class="fa fa-globe pr-10"></i>Mars city, Country </p>
+
                     <p><i class="fa fa-mobile pr-10"></i>Mobile : (123) 456-7890 </p>
+
                     <p><i class="fa fa-phone pr-10"></i>Phone : (123) 456-7890 </p>
-                    <p><i class="fa fa-envelope pr-10"></i>Email :   <a href="javascript:;">support@example.com</a></p>
+
+                    <p><i class="fa fa-envelope pr-10"></i>Email : <a href="javascript:;">support@example.com</a></p>
                 </address>
             </div>
             <div class="col-lg-3 col-sm-3 wow fadeInUp" data-wow-duration="2s" data-wow-delay=".3s">
                 <h1>latest tweet</h1>
+
                 <div class="tweet-box">
                     <i class="fa fa-twitter"></i>
                     <em>
@@ -81,6 +86,7 @@
                     <h1>
                         Text Widget
                     </h1>
+
                     <p>
                         This is a text widget.Lorem ipsum dolor sit amet.
                         This is a text widget.Lorem ipsum dolor sit amet.
@@ -97,14 +103,22 @@
         <div class="row">
             <div class="col-lg-6 col-sm-6 pull-right">
                 <ul class="social-link-footer list-unstyled">
-                    <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".1s"><a href="#"><i class="fa fa-facebook"></i></a></li>
-                    <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".2s"><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                    <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".3s"><a href="#"><i class="fa fa-dribbble"></i></a></li>
-                    <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".4s"><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                    <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".5s"><a href="#"><i class="fa fa-twitter"></i></a></li>
-                    <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".6s"><a href="#"><i class="fa fa-skype"></i></a></li>
-                    <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".7s"><a href="#"><i class="fa fa-github"></i></a></li>
-                    <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".8s"><a href="#"><i class="fa fa-youtube"></i></a></li>
+                    <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".1s"><a href="#"><i
+                                class="fa fa-facebook"></i></a></li>
+                    <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".2s"><a href="#"><i
+                                class="fa fa-google-plus"></i></a></li>
+                    <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".3s"><a href="#"><i
+                                class="fa fa-dribbble"></i></a></li>
+                    <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".4s"><a href="#"><i
+                                class="fa fa-linkedin"></i></a></li>
+                    <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".5s"><a href="#"><i
+                                class="fa fa-twitter"></i></a></li>
+                    <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".6s"><a href="#"><i
+                                class="fa fa-skype"></i></a></li>
+                    <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".7s"><a href="#"><i
+                                class="fa fa-github"></i></a></li>
+                    <li class="wow flipInX" data-wow-duration="2s" data-wow-delay=".8s"><a href="#"><i
+                                class="fa fa-youtube"></i></a></li>
                 </ul>
             </div>
             <div class="col-md-4">
@@ -117,51 +131,50 @@
 </footer>
 <!--small footer end-->
 
-<script src="webresources/js/jquery-1.8.3.min.js">
+<script src="webresources/js/jquery-1.8.3.min.js" type="text/javascript">
 </script>
-<script src="webresources/js/bootstrap.min.js">
+<script src="webresources/js/bootstrap.min.js" type="text/javascript">
 </script>
-<script type="text/javascript" src="webresources/js/hover-dropdown.js">
+<script src="webresources/js/hover-dropdown.js" type="text/javascript">
 </script>
-<script defer src="webresources/js/jquery.flexslider.js">
+<script defer src="webresources/js/jquery.flexslider.js" type="text/javascript">
 </script>
-<script type="text/javascript" src="webresources/assets/bxslider/jquery.bxslider.js">
-</script>
-
-<script type="text/javascript" src="webresources/js/jquery.parallax-1.1.3.js">
-</script>
-<script src="webresources/js/wow.min.js">
-</script>
-<script src="webresources/assets/owlcarousel/owl.carousel.js">
+<script src="webresources/assets/bxslider/jquery.bxslider.js" type="text/javascript">
 </script>
 
-<script src="webresources/js/jquery.easing.min.js">
+<script  src="webresources/js/jquery.parallax-1.1.3.js" type="text/javascript">
 </script>
-<script src="webresources/js/link-hover.js">
+<script src="webresources/js/wow.min.js" type="text/javascript">
 </script>
-<script src="webresources/js/superfish.js">
+<script src="webresources/assets/owlcarousel/owl.carousel.js" type="text/javascript">
 </script>
-<script type="text/javascript" src="webresources/js/parallax-slider/jquery.cslider.js">
+
+<script src="webresources/js/jquery.easing.min.js" type="text/javascript">
+</script>
+<script src="webresources/js/link-hover.js" type="text/javascript">
+</script>
+<script src="webresources/js/superfish.js" type="text/javascript">
+</script>
+<script  src="webresources/js/parallax-slider/jquery.cslider.js" type="text/javascript">
 </script>
 <script type="text/javascript">
-    $(function() {
+    $(function () {
 
         $('#da-slider').cslider({
-            autoplay    : true,
-            bgincrement : 100
+            autoplay: true,
+            bgincrement: 100
         });
 
     });
 </script>
 
 
-
 <!--common script for all pages-->
-<script src="js/common-scripts.js">
+<script src="webresources/js/common-scripts.js" type="text/javascript">
 </script>
 
 <script type="text/javascript">
-    jQuery(document).ready(function() {
+    jQuery(document).ready(function () {
 
 
         $('.bxslider1').bxSlider({
@@ -186,26 +199,26 @@
 <script>
     $('a.info').tooltip();
 
-    $(window).load(function() {
+    $(window).load(function () {
         $('.flexslider').flexslider({
             animation: "slide",
-            start: function(slider) {
+            start: function (slider) {
                 $('body').removeClass('loading');
             }
         });
     });
 
-    $(document).ready(function() {
+    $(document).ready(function () {
 
         $("#owl-demo").owlCarousel({
 
-            items : 4
+            items: 4
 
         });
 
     });
 
-    jQuery(document).ready(function(){
+    jQuery(document).ready(function () {
         jQuery('ul.superfish').superfish();
     });
 
