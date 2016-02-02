a2984168158272e6bc57fb7c9087aa67ba03dad5
hoanvokim <TihHon@16LH>
2016-02-02 23:42:37 +0700

    update main layout

--- webapps/views/pages/home.php
+++ webapps/views/pages/home.php
@@ -12,25 +12,26 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <h2>
-                        <span>Certificates</span>
-                        <br>
-                        <span>Quality</span>
-                        <br>
-                        <span>Trusted</span>
-                    </h2>
+                    <div class="da-slide-content">
+                        <h2>
+                            <span>Certificates</span>
+                            <br>
+                            <span>Quality</span>
+                            <br>
+                            <span>Trusted</span>
+                        </h2>
 
-                    <p>
-                        <span>To get a Prized from a lot of </span>
-                        <br/>
-                        <span>organization from trusted to make sure</span>
-                        <br/>
-                        <span>the best products we do.</span>
-                    </p>
-                    <a href="#" class="btn btn-info btn-lg da-link">
-                        Read more
-                    </a>
-
+                        <p>
+                            <span>To get a Prized from a lot of </span>
+                            <br/>
+                            <span>organization from trusted to make sure</span>
+                            <br/>
+                            <span>the best products we do.</span>
+                        </p>
+                        <a href="#" class="btn btn-info btn-lg da-link">
+                            Read more
+                        </a>
+                    </div>
                     <div class="da-img">
                         <img src="webresources/certificates/merged-1.png" alt="image01"/>
                     </div>
@@ -113,7 +114,7 @@
         <div class="col-md-12">
             <div class="text-center feature-head wow fadeInDown">
                 <h1 class="">
-                    welcome to Acme
+                    The latest news
                 </h1>
 
             </div>
@@ -123,44 +124,50 @@
                 <div class="col-md-4 col-sm-4 text-center wow fadeInUp">
                     <div class="feature-box-heading">
                         <em>
-                            <img src="webresources/img/1.png" alt="" width="100" height="100">
+                            <img src="webresources/img/files/pdf.png" alt="" width="64" height="64">
 
                         </em>
-                        <h4>
-                            <b>Multipurpose Template</b>
-                        </h4>
+                        <h3>
+                            Review of Administration in 2015
+                        </h3>
                     </div>
                     <p class="text-center">
-                        Lorem ipsum dolor sit amet, dolore eiusmod quis tempor incididunt ut et dolore Ut veniam unde
-                        nostrudlaboris.
+                        Download the file to read the review of administration in 2015.
+                    </p>
+                    <p class="text-right dark-text">
+                        09-01-2016 10:37am
                     </p>
                 </div>
                 <div class="col-md-4 col-sm-4 text-center wow fadeInUp">
                     <div class="feature-box-heading">
                         <em>
-                            <img src="webresources/img/2.png" alt="" width="100" height="100">
+                            <img src="webresources/img/files/rating.png" alt="" width="64" height="64">
                         </em>
-                        <h4>
-                            <b>Well Documented</b>
-                        </h4>
+                        <h3>
+                            The best export companies in 2014
+                        </h3>
                     </div>
                     <p class="text-center">
-                        Lorem ipsum dolor sit amet, dolore eiusmod quis tempor incididunt ut et dolore Ut veniam unde
-                        nostrudlaboris.
+                        These companies of the list below are the best company of export from US.
+                    </p>
+                    <p class="text-right dark-text">
+                        09-10-2015 10:37am
                     </p>
                 </div>
                 <div class="col-md-4 col-sm-4 text-center wow fadeInUp">
                     <div class="feature-box-heading">
                         <em>
-                            <img src="webresources/img/3.png" alt="" width="100" height="100">
+                            <img src="webresources/img/files/law.png" alt="" width="64" height="64">
                         </em>
-                        <h4>
-                            <b>Responsive Design</b>
-                        </h4>
+                        <h3>
+                            New laws
+                        </h3>
                     </div>
                     <p class="text-center">
-                        Lorem ipsum dolor sit amet, dolore eiusmod quis tempor incididunt ut et dolore Ut veniam unde
-                        nostrudlaboris.
+                        New laws of Operation and Organization. (edited in 2015)
+                    </p>
+                    <p class="text-right dark-text">
+                        09-07-2015 10:37am
                     </p>
                 </div>
             </div>
