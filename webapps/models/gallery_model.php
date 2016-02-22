b2bc285ff470cb9bfa7de34c0238af8c5f9415f9
Tran Dinh Nhu <N.Tran@laudert.de>
2016-02-22 14:35:20 +0700

    gallery model

--- webapps/models/gallery_model.php
+++ webapps/models/gallery_model.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Created by IntelliJ IDEA.
  * User: NhuTran
@@ -6,3 +7,56 @@
  * Time: 1:40 AM
  * To change this template use File | Settings | File Templates.
  */
+class Gallery_Model extends CI_Model
+{
+
+    public function __construct()
+    {
+        $this->load->database();
+    }
+
+    public function findAll()
+    {
+        $query = $this->db->get('gallery');
+        return $query->result_array();
+    }
+
+    public function findById($id)
+    {
+
+    }
+
+    public function findByGroup($group)
+    {
+        $this->db->where('group', $group);
+        return $this->db->get('gallery')->result_array();
+    }
+
+    public function insert($name, $url_image, $group)
+    {
+        $data = array(
+            'name' => $name,
+            'url_image' => $url_image,
+            'group' => $group
+        );
+
+        $this->db->insert('gallery', $data);
+    }
+
+    public function update($name, $url_image, $group)
+    {
+        $data = array(
+            'name' => $name,
+            'url_image' => $url_image,
+            'group' => $group
+        );
+
+        $this->db->update('gallery', $data);
+    }
+
+    public function delete($id)
+    {
+        $this->db->where('id', $id);
+        $this->db->delete('gallery');
+    }
+}
