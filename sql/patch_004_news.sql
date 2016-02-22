beb8b5f9abd22b54013bfbdb848fe9d0f517eeca
Tran Dinh Nhu <N.Tran@laudert.de>
2016-02-22 15:31:34 +0700

    news table

--- sql/patch_004_news.sql
+++ sql/patch_004_news.sql
@@ -2,6 +2,7 @@ CREATE TABLE `news` (
     `id`                TINYINT       NOT NULL AUTO_INCREMENT,
     `title`             NVARCHAR(100) NOT NULL,
     `content`           TEXT,
+    `url_image`         NVARCHAR(250),
     `url_attached_file` NVARCHAR(250),
     `type`              NVARCHAR(100),
     PRIMARY KEY (`id`)
