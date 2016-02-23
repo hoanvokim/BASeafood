b4cdb0dc0b3a7a58c59506123b927144e58ea4f4
NhuTran <trandinhnhubk@gmail.com>
2016-02-23 20:37:05 +0700

    Product management:  - Display products  - Create product follow specific category

--- sql/patch_003_products.sql
+++ sql/patch_003_products.sql
@@ -4,12 +4,9 @@ CREATE TABLE `product` (
   `en_name` nvarchar(100) NOT NULL,
   `size` nvarchar(100) NOT NULL,
   `packing` nvarchar(100) NOT NULL,
-  `fk_image`    TINYINT,
+  `url` nvarchar(250) not null,
   `fk_category` TINYINT,
   PRIMARY KEY (`id`),
-  INDEX `product_image_index` (`fk_image`),
-  FOREIGN KEY (`fk_image`)
-  REFERENCES `images` (`id`),
   INDEX `product_category_index` (`fk_category`),
   FOREIGN KEY (`fk_category`)
   REFERENCES `category` (`id`)
