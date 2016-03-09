8c87a7a1cdc410fa9c9787d0b55d164e36fcf297
Tran Dinh Nhu <N.Tran@laudert.de>
2016-03-09 15:39:38 +0700

    update news

--- webapps/controllers/dm/manager_news_controller.php
+++ webapps/controllers/dm/manager_news_controller.php
@@ -65,6 +65,49 @@ class Manager_news_controller extends CI_Controller
         $this->load->view('pages/admin/news/create', $data);
     }
 
+    public function update()
+    {
+        if (!$this->is_login()) {
+            $this->load_login_view();
+            return;
+        }
+        $data['title'] = 'Edit news';
+        $data['error'] = '';
+        $news = $this->news_model->findById($this->uri->segment(3));
+        if ($news) {
+            $data['news'] = $news;
+            $this->load->view('pages/admin/news/update', $data);
+            return;
+        }
+        redirect('news-manager', 'refresh');
+    }
+
+    public function post_update()
+    {
+        if (!$this->is_login()) {
+            $this->load_login_view();
+            return;
+        }
+        $data['title'] = 'Edit news';
+        $this->load->library('form_validation');
+        $this->load->library('upload', $this->get_config());
+        if ($this->upload->do_upload('userfile')) {
+            $upload_files = $this->upload->data();
+            $file_path = 'assets/upload/images/news/' . $upload_files['file_name'];
+            $id = $this->input->post('nid');
+            $en_title = $this->input->post('en_title');
+            $vi_title = $this->input->post('vi_title');
+            $en_content = $this->input->post('en_content');
+            $vi_content = $this->input->post('vi_content');
+            $type = $this->input->post('type');
+            $this->news_model->update($id, $en_title, $vi_title, $en_content,
+                $vi_content, $file_path, $file_path, $type);
+            redirect('news-manager', 'refresh');
+        }
+        $data['error'] = $this->upload->display_errors();
+        $this->load->view('pages/admin/news/update', $data);
+    }
+
     private function get_config()
     {
         return array(
