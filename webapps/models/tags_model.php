c8093d39e4b1ef5aee826798fd2df98f8f775205
Tran Dinh Nhu <N.Tran@laudert.de>
2016-03-23 11:42:04 +0700

    tags model

--- webapps/models/tags_model.php
+++ webapps/models/tags_model.php
@@ -58,6 +58,60 @@ class Tags_Model extends CI_Model
         $this->db->insert('tags', $data);
     }
 
+    public function saveReferenceProduct($tag, $product)
+    {
+        $data = array(
+            'fk_product' => $product,
+            'fk_tags' => $tag,
+        );
+        $this->db->insert('product_tags', $data);
+    }
+
+    public function saveReferenceNews($tag, $news)
+    {
+        $data = array(
+            'fk_news' => $news,
+            'fk_tags' => $tag,
+        );
+        $this->db->insert('news_tags', $data);
+    }
+
+    public function findProductsByViTags($vi_name)
+    {
+        $sql = 'select p.* from product as p
+                join product_tags as pt on pt.fk_product=p.id
+                JOIN tags as t on t.id=pt.fk_tags
+                WHERE t.vi_name = ' . $vi_name;
+        return $this->db->query($sql)->result_array();
+    }
+
+    public function findProductsByEnTags($en_name)
+    {
+        $sql = 'select p.* from product as p
+                join product_tags as pt on pt.fk_product=p.id
+                JOIN tags as t on t.id=pt.fk_tags
+                WHERE t.en_name = ' . $en_name;
+        return $this->db->query($sql)->result_array();
+    }
+
+    public function findNewsByViTags($vi_name)
+    {
+        $sql = 'select p.* from news as p
+                join news_tags as pt on pt.fk_news=p.id
+                JOIN tags as t on t.id=pt.fk_tags
+                WHERE t.vi_name = ' . $vi_name;
+        return $this->db->query($sql)->result_array();
+    }
+
+    public function findNewsByEnTags($en_name)
+    {
+        $sql = 'select p.* from news as p
+                join news_tags as pt on pt.fk_news=p.id
+                JOIN tags as t on t.id=pt.fk_tags
+                WHERE t.en_name = ' . $en_name;
+        return $this->db->query($sql)->result_array();
+    }
+
     public function update($id, $en_name, $vi_name)
     {
         $data = array(
