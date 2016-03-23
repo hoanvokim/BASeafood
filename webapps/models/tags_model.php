b34aaa96113df142b6240eaa5f61f6392aa0d80c
Tran Dinh Nhu <N.Tran@laudert.de>
2016-03-23 11:25:43 +0700

    tags model

--- webapps/models/tags_model.php
+++ webapps/models/tags_model.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Created by IntelliJ IDEA.
  * User: NhuTran
@@ -6,3 +7,70 @@
  * Time: 1:42 AM
  * To change this template use File | Settings | File Templates.
  */
+class Tags_Model extends CI_Model
+{
+    public function __construct()
+    {
+        $this->load->database();
+    }
+
+    public function findAll()
+    {
+        return $this->db->get("tags")->result_array();
+    }
+
+    public function findByViName($vi_name)
+    {
+        $this->db->where('vi_name =', $vi_name);
+        $query = $this->db->get('tags')->result_array();
+        if ($query) {
+            foreach ($query as $rc) {
+                return array(
+                    'id' => $rc['id'],
+                    'vi_name' => $rc['vi_name'],
+                    'en_name' => $rc['en_name']
+                );
+            }
+        }
+    }
+
+    public function findByEnName($en_name)
+    {
+        $this->db->where('en_name =', $en_name);
+        $query = $this->db->get('tags')->result_array();
+        if ($query) {
+            foreach ($query as $rc) {
+                return array(
+                    'id' => $rc['id'],
+                    'vi_name' => $rc['vi_name'],
+                    'en_name' => $rc['en_name']
+                );
+            }
+        }
+    }
+
+    public function insert($en_name, $vi_name)
+    {
+        $data = array(
+            'en_name' => $en_name,
+            'vi_name' => $vi_name,
+        );
+        $this->db->insert('tags', $data);
+    }
+
+    public function update($id, $en_name, $vi_name)
+    {
+        $data = array(
+            'en_name' => $en_name,
+            'vi_name' => $vi_name,
+        );
+        $this->db->where('id', $id);
+        $this->db->update('tags', $data);
+    }
+
+    public function delete($id)
+    {
+        $this->db->where('id', $id);
+        $this->db->delete('tags');
+    }
+}
